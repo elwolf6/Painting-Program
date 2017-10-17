0e70ec0227d7f83a6e44c395cfe4937e3507b0c9
Luis Marroquin <luigi.marroquin.2005@gmail.com>
2017-10-16 21:10:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,8 +20,10 @@ function draw() {
 	textSize(120)
 	text("elwolf's painting Program",210,100)
 	rect(0,130,10000,50)
-	if (mouseIsPressed) {
-		ellipse(mouseX,mouseY,BrushSize,BrushSize);
-	}
 }
 
+function mouseDragged() 
+{ 
+	strokeWeight(10);
+	line(mouseX, mouseY, BrushSize, BrushSize);
+}
