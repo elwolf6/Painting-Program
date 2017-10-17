a42f0bba6a2fc83e006d0111194447bc0d0252d0
Luis Marroquin <luigi.marroquin.2005@gmail.com>
2017-10-16 21:18:28 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,16 @@ function setup() {
  
 function draw()
 {
-	// do nothing
+	//BrushSize = 30
+	fill(0,0,0);
+	noStroke()
+	textSize(40)
+	text("Brush Size:",210,210)
+	//textSize(40)
+	//text(BrushSize,210,250)
+	textSize(120)
+	text("elwolf's painting Program",210,100)
+	rect(0,130,10000,50)
 }
  
 function mouseDragged() 
