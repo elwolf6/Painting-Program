e417e30ae87da9f18aaadc7d6e14bc5c2a5e0536
Luis Marroquin <luigi.marroquin.2005@gmail.com>
2017-10-16 20:27:54 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+var BrushSize
+
+//function preload() {
+	//img = loadImage("assets/DeathTaco.png")
+//}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	BrushSize = 30
+	fill(0,0,0);
+	noStroke()
+	textSize(40)
+	text("Brush Size:",210,210)
+	textSize(40)
+	text(BrushSize,210,250)
+	textSize(120)
+	text("elwolf's painting Program",210,100)
+	rect(0,130,10000,50)
+	if (keyIsDown(UP_ARROW))
+    BrushSize = BrushSize + 10;
+	if (keyIsDown(DOWN_ARROW))
+    BrushSize = BrushSize - 10;
+	if (mouseIsPressed) {
+		ellipse(mouseX,mouseY,BrushSize,BrushSize);
+	}
+}
+
+function keyPressed() {
+  if (keyCode == 0x52) {
+	noStroke();
+    fill(255,255,255);
+	rect(0,0,1920,1080);
+  }
+  if (keyCode == 0x45) {
+	noStroke();
+    fill(255,255,255);
+	rect(mouseX,mouseY,30,30);
+  }
+  return false; // prevent default
+}
