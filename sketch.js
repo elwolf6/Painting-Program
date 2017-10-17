1c0a297e39ca6891833a98e7a0f2097c194ee168
Luis Marroquin <luigi.marroquin.2005@gmail.com>
2017-10-16 21:15:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,12 @@
-
-var BrushSize
-
-//function preload() {
-	//img = loadImage("assets/DeathTaco.png")
-//}
-
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+ 	createCanvas(400, 400);
 }
-
-function draw() {
-	BrushSize = 40
-	fill(0,0,0);
-	noStroke()
-	textSize(40)
-	text("Brush Size:",210,210)
-	textSize(40)
-	text(BrushSize,210,250)
-	textSize(120)
-	text("elwolf's painting Program",210,100)
-	rect(0,130,10000,50)
+ 
+function draw()
+{
+	// do nothing
 }
-
+ 
 function mouseDragged() 
 { 
 	strokeWeight(10);
